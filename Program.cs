e69ddd04110cffcc0a576fa14c6090fddb18b59f
IU223 <2471636594@qq.com>
2025-07-21 13:43:41 +0800

    分支提交

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("hello");
           Console.WriteLine("Hello, World!");
         }
     }
